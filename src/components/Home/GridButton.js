79527219e7718ad50ac80187408bf0c6bf266736
Sarah Beaubeau <sarah.beaubeau@hotmail.fr>
2021-10-04 14:43:10 +0200

    Ajouts des boutons, grille, style et props isPlayed

---
+++ src/components/Home/GridButton.js
@@ -0,0 +1,38 @@
+import styled from "styled-components";
+
+export default function GridButton({ isPlayed = false }) {
+    return (
+        <Wrapper isPlayed={isPlayed}>
+
+        </Wrapper>
+    );
+}
+
+const Wrapper = styled.div`
+border-radius: 5px;
+background: #7952B3;
+background: radial-gradient(circle, #A5E1AD 0%, #7952B3 100%);
+position: relative;
+&::before{
+    content: "";
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    z-index: 0;
+    background: radial-gradient(
+        circle, 
+        #EDFFA9 ${(props)=>(props.isPlayed ? "20%" : "0%")}, 
+        #AE00FB 100%);
+    opacity: ${(props)=>(props.isPlayed ? "1" : "0")};
+    transition: linear 0.2s;
+}
+&:hover::before{
+    opacity: 1;
+}
+&:active::before{
+    opacity: 1;
+    background: radial-gradient(circle, #EDFFA9 10%, #AE00FB 80%);
+}
+`;
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,11 +1,36 @@
 import styled from "styled-components";
 
+import GridButton from "./GridButton";
+
+const list = ["el1", "el2", "el3", "el4", "el5", "el6", "el7", "el8", "el9"];
+
 export default function Home() {
     return(
         <Wrapper>
-            <h1>HOME</h1>
+            <Grid>
+                {list.map((el)=>{
+                    return <GridButton key={el}>{el}</GridButton>
+                })}
+            </Grid>
         </Wrapper>
     );
 }
 
 const Wrapper = styled.div``;
+
+const Grid = styled.div`
+display: grid;
+grid-template-columns: 1fr 1fr 1fr;
+column-gap: 12px;
+row-gap: 12px;
+margin: auto;
+padding: 24px;
+width: 900px;
+height: 900px;
+border: solid 1px aqua;
+
+@media(max-width : 640px) {
+    width: 300px;
+    height: 300px;
+}
+`;
